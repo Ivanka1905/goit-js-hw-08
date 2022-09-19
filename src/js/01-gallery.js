eabf8a2856e6f6a924c0780a70ae37ec3e61d0f7
Ivanka Nagornyuk <IvNagornyuk@gmail.com>
2022-09-19 18:13:58 +0300

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+galleryElem.insertAdjacentHTML('afterbegin', markup);
+function createMarkup(items) {
+    return items.map(item => {
+        return `
+  <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    }).join('');
+    
+};
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'ALT', captionDelay: 250,
+    });
+    console.log(lightbox)
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from "@vimeo/player";
+import Throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const timeKey = 'videoplayer-current-time';
+function saveTimeToLocal({ seconds }) {
+    localStorage.setItem(timeKey, seconds);
+};
+window.addEventListener("load", afterReloading);
+player.on('timeupdate', Throttle(saveTimeToLocal, 1000));
+function afterReloading() {
+    if (!localStorage.getItem(timeKey)) { return };
+    const currentVideoTime = localStorage.getItem(timeKey)
+    player.setCurrentTime(currentVideoTime).then(() => { player.play() }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+}
+
+// done
